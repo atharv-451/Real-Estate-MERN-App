2e14a39b24029a31e86ef12133fb66d9da6c8a00
atharv-451 <atharv451@gmail.com>
2024-02-15 17:12:34 +0530

    About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,14 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About EstateNest</h1>
+      <p className="mb-4 text-slate-700">At EstateNest, we believe in transforming your real estate dreams into reality. With a commitment to excellence and a passion for helping our clients find their perfect homes, we provide a seamless and personalized experience every step of the way.</p>
+      <p className="mb-4 text-slate-700">Our platform serves as your trusted partner in navigating the complexities of the real estate market. Whether you&#39;re searching for a cozy apartment in the heart of the city, a spacious family home in the suburbs, or a luxurious beachfront villa, EstateNest offers a diverse range of properties to suit every lifestyle and budget.</p>
+      <p className="mb-4 text-slate-700">With a user-friendly interface and advanced search functionalities, finding your ideal property has never been easier. Our team of experienced real estate professionals is dedicated to understanding your unique needs and preferences, ensuring that you find a home that truly resonates with you.</p>
+      <p className="mb-4 text-slate-700">Beyond just listings, EstateNest is a hub for valuable insights and resources to empower you on your real estate journey. From market trends and neighborhood guides to financing options and legal advice, we provide the information you need to make informed decisions with confidence.</p>
+      <p className="mb-4 text-slate-700">Whether you&#39;re a first-time homebuyer, seasoned investor, or looking to sell your property, EstateNest is here to support you every step of the way. Welcome to EstateNest, where your dream home awaits. Discover the possibilities today and embark on a journey towards a brighter future with us.</p>
+    </div>
   )
 }
